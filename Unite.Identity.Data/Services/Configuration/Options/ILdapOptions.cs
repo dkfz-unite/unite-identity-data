aa9b3755745b86a32af723294226e29ef263fa5f
DGitRich <daniel.rich@gmx.net>
2023-06-29 16:46:53 +0200

    Create ILdapOptions

---
+++ Unite.Identity.Data/Services/Configuration/Options/ILdapOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Unite.Identity.Data.Services.Configuration.Options;
+
+public interface ILdapOptions
+{
+    string Server { get; }
+    string Port { get; }
+    string UserTargetOU { get; }
+    string ServiceUserRNA { get; }
+    string ServiceUserPassword { get; }
+}
